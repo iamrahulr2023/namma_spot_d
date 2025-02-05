255c128abb9dd20a5e832c9d23bc16f97dfe524c
iamrahulr2023 <iamrahul.r.2005@gmail.com>
2025-02-05 15:32:29 +0530

    by thamil

--- server_new/server/modules/Notification.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const DetailsSchema = new mongoose.Schema({
-   noti:String,
-   date:String,
- 
-});
-
-module.exports = mongoose.model('notification', DetailsSchema);
--- server_new/server/modules/employee.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const employeeSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  pass: { type: String, required: true }
-});
-
-module.exports = mongoose.model('loginusers', employeeSchema);
